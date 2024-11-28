0cfe10a87bc207677b7a9274b5c26b92c905231e
Splincer <entryone722@gmail.com>
2024-11-28 08:38:24 -0500

    Add files via upload

---
+++ last_day.js
@@ -0,0 +1,13 @@
+addIngredient("chocolate balls",{
+    color:"#5e3100",
+    innerColor:"#5e3100",
+    type:"chocolate",
+    shape:"circle_pile"
+});
+
+addIngredient("human face",{
+    color:"#f7c994", "#4a2700", "#8f693d", "#fcd6a9"
+    innerColor:"#a10300",
+    type:"fruit",
+    shape:"face"
+});
